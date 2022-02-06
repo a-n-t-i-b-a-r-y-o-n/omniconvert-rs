acbb84f98b97620872915bbbf24bd4b61f66477b
g-r-a-v-i-t-y-w-a-v-e <77591737+a-n-t-i-b-a-r-y-o-n@users.noreply.github.com>
2022-02-06 16:19:48 -0600

    Fix issue with u32 pointer increment magic

--- src/magic.rs
+++ src/magic.rs
@@ -17,11 +17,11 @@ pub fn add_u8_overflow(a: u8, b: u8) -> u8 {
     (a as u16 + b as u16) as u8
 }
 
-// Emulate C raw pointer increment logic to carve up input
-pub fn u32_pointer_increment(input: &Vec<u32>, offset: u32) -> u32 {
+// Emulate C raw pointer increment logic to carve up input at byte offsets
+pub fn u32_pointer_increment(input: &Vec<u32>, offset_bytes: u32) -> u32 {
     // Start index to read at
-    let base = (offset/4) as usize;
-    match offset % 4 {
+    let base = (offset_bytes/4) as usize;
+    match offset_bytes % 4 {
         0 => {
             // Offset points to the beginning of a u32
             // Return the whole u32
@@ -30,13 +30,12 @@ pub fn u32_pointer_increment(input: &Vec<u32>, offset: u32) -> u32 {
         r => {
             // Offset points to the middle of a u32.
             // This will require adding (4-r) bytes of u32 #1 to r bytes of u32 #2
-            // All the (r*2) operations are to account for the hex notation
 
             // TODO: Research how raw u32 pointer increment even works!
             // Mask for 1st u32
-            let a = (input[base] & (0xFFFFFFFF >> (r*2))) << (r*2);
+            let a = (input[base] & (0xFFFFFFFF >> (r*8))) << (r*8);
             // Mask for 2nd u32
-            let b = (input[base+1] & (0xFFFFFFFF << (r*2))) >> (r*2);
+            let b = (input[base+1] & (0xFFFFFFFF << (r*8))) >> (r*8);
 
             a + b
         }
